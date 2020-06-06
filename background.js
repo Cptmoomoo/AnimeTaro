bea7fbc36669d5b6bdbf3d771e160799a688d9c3
Cptmoomoo <45883132+Cptmoomoo@users.noreply.github.com>
2020-06-06 15:04:29 -0600

    tab switch/popup modifications

--- background.js
+++ background.js
@@ -4,19 +4,35 @@ chrome.browserAction.onClicked.addListener(function(tab) {
         chrome.tabs.sendMessage(activeTab.id, {"action": "clicked_browser_action"});
     });
 });
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    chrome.tabs.sendMessage(activeInfo.tabId, {"action": "tab_changed"});
+});
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        var is_on_streaming = false;
+        var currentDomain;
+        var currentPath;
 
-// On page load, check to see if page not found, if not try another source.
+        if (request.action === "tabswitch") {
+            if (request.tabURL !== "myanimelist.net") {
+                chrome.browserAction.setPopup({popup: "popup.html"});
+            }
+            else {
+                chrome.browserAction.setPopup({popup: ""});
+            }
+        }
 
-// You thought this would be a comment, but it's me, DIO!
+        else if (request.action === "pageloaded") {
+            currentDomain = request.pageinfo[0];
+            currentPath = request.pageinfo[1];
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if(request.action === "find_source") {
-
-            // check for search query
             // unicode characters? - ☆
 
-            var url_parsed = request.url.split("/");
+            var url_parsed = currentPath;
+            if (url_parsed.includes("?")) {
+                url_parsed = url_parsed.split("?");
+                url_parsed.pop();
+            }
+            url_parsed = url_parsed.split("/");
             var mal_title = url_parsed[url_parsed.length - 1];
             mal_title = mal_title.toLowerCase();
             var anime_title = mal_title.replace(/__/g, "-");
@@ -26,7 +42,7 @@ chrome.runtime.onMessage.addListener(
             chrome.storage.sync.get({prefsource: 'gogo'}, function(result) {
                 var user_op = result.prefsource;
 
-                // Add 10% chance for dio
+                    // Add 10% chance for dio
 
                 if (user_op === "gogo") {
                     new_url = "https://www6.gogoanimehub.tv/category/" + anime_title;
@@ -34,7 +50,7 @@ chrome.runtime.onMessage.addListener(
                 else {
                     new_url = "https://www.kiss-anime.ws/Anime/" + anime_title;
                 }
-                chrome.tabs.create({"url": new_url});
+                chrome.tabs.create({url: new_url, active: false});
             });
         }
     });
--- content.js
+++ content.js
@@ -1,6 +1,12 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    var pathname = window.location.pathname;
+    var hostURL = window.location.hostname;
+
     if (request.action === "clicked_browser_action") {
-        var currentURL = window.location.pathname;
-        chrome.runtime.sendMessage({"action": "find_source", "url": currentURL});
+        chrome.runtime.sendMessage({"action": "pageloaded", "pageinfo": [hostURL, pathname]});
     }
+    else if (request.action === "tab_changed") {
+        chrome.runtime.sendMessage({"action": "tabswitch", "tabURL": hostURL});
+    }
+
 });
